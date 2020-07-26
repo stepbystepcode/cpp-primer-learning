25a40aa890c21c36d127b19610c32664a1d0c043
ybygjylj <abc1870819884@163.com>
2020-07-26 12:47:30 +0800

    fix problem

--- 1.4/test7.cpp
+++ 1.4/test7.cpp
@@ -3,7 +3,7 @@ int main(){
   int num1 = 0, num2 = 0;
   std::cin >> num1
            >> num2;
-  for (num1; num1 <= num2; ++num1){
+  for (; num1 <= num2; ++num1){
     std::cout << num1 << std::endl;
   }
   return 0;
