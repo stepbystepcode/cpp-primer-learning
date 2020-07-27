ddcdc7ee771dd9be75adca92d161a0e98605a25d
ybygjylj <abc1870819884@163.com>
2020-07-27 14:03:56 +0800

    this

--- 1.4/test1-23.cpp
+++ 1.4/test1-23.cpp
@@ -13,3 +13,24 @@ int main()
     return -1; //faild
   }
 }
+#include <iostream>
+int main()
+{
+  int currVal = 0, val = 0;
+  if (std::cin >> currVal) {
+    int cnt = 1;
+    while (std::cin >> val) {
+      if (val == currVal)
+        ++cnt;
+      else {
+        std::cout << currVal << " occurs "
+                  << cnt << " times " << std::endl;
+        currVal = val;
+        cnt = 1;
+      }
+    }
+    std:: cout << currVal << " occurs "
+               << cnt << " times " << std::endl;
+  }
+  return 0;
+}
