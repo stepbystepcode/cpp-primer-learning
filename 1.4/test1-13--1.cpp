cf2f9d774d3708874bae7c54926001e3bf3e0912
ybygjylj <abc1870819884@163.com>
2020-07-27 17:15:21 +0800

    update

--- 1.4/test1-13--1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int sum = 0;
-int main(){
-  for (int i = 50; i <=100; ++i){
-    sum += i;
-  }
-  std::cout << sum << std::endl;
-  return 0;
-}
--- 1.4/test1-13--2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main(){
-  for (int i = 10; i >= 0; --i){
-    std::cout << i << std::endl;
-  }
-  return 0;
-}
